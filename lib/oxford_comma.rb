c753082d4b0ce866fef2bc865967821063172cd4
icstr8thruyoo <mariamorton1206@gmail.com>
2020-12-20 21:00:47 -0500

    commit tree 24b3b8d56c56b397b34a418a98aba313aa4b2e23

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    new_array = []
+    if array.length < 2
+      array.join
+    elsif array.length == 2
+      array.join(" and ")
+    else
+      array.each do |element|
+        if array.index(element) == 0
+          new_array << element        
+        elsif array.index(element) > 0 && array.index(element) < (array.length [-1])
+          new_array << ", #{element}"
+        else
+          new_array << ", and #{element}"
+        end
+    end
 end
+
+  
